1faabcf9f5e4083bfe6b154fd8441df8a7029b3c
JusticeW4ever <1197741562@qq.com>
2023-09-04 02:39:02 +0800

    Update Leetcode_2511.java

--- src/main/java/leetcode/Leetcode_2511.java
+++ src/main/java/leetcode/Leetcode_2511.java
@@ -3,6 +3,10 @@ package leetcode;
 /**
  * @author BlackVJack/Justice
  * @time 2023/9/3 20:21
+ *
+ * 力扣题库第2511题 最多可以摧毁的敌人城堡数目
+ * 题目太长:
+ *  题目链接:https://leetcode.cn/problems/maximum-enemy-forts-that-can-be-captured/?envType=daily-question&envId=2023-09-02
  */
 
 
