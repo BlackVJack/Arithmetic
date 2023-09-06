8f902e57bd3afb9640e50b0c9fc289b38bc6b006
JusticeW4ever <1197741562@qq.com>
2023-09-07 01:20:50 +0800

    Update Leetcode_0028.java

--- src/main/java/leetcode/Leetcode_0028.java
+++ src/main/java/leetcode/Leetcode_0028.java
@@ -10,6 +10,33 @@ package leetcode;
 
 public class Leetcode_0028 {
 
+
+public int strStr(String haystack, String needle) {
+    int length_1 = haystack.length();
+    int length_2 = needle.length();
+
+    if (length_2 > length_1) return -1;
+    if (length_1 == length_2) {
+    if (haystack.equals(needle)) return 0;
+        return -1;
+    }
+
+    int i = 0;
+    while (i < length_1 - length_2 + 1) {
+        int j = 0;
+        while(j < length_2) {
+            if (haystack.charAt(i + j) != needle.charAt(j)) {
+                break;
+            }
+            j++;
+        }
+        if (j == length_2) return i;
+        i++;
+    }
+    return -1;
+}
+
+
     public static void main(String[] args) {
         java.text.NumberFormat nf = java.text.NumberFormat.getInstance();
         nf.setGroupingUsed(false);      //关闭科学计数法
